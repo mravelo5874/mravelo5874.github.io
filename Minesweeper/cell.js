609a20e4c983e6f1005d91f04070ac5f1ef0c8c5
Marco <mravelo5874@tamu.edu>
2018-10-21 23:07:47 -0500

    added game

---
+++ Minesweeper/cell.js
@@ -0,0 +1,105 @@
+function Cell(i, j, w)
+{
+	this.i = i;
+	this.j = j;
+	this.x = i*w;
+	this.y = j*w + 30;
+	this.w = w;
+	this.bee = false;
+	this.open = false;
+	this.beesNext = 0;
+	this.flag = false;
+}
+
+Cell.prototype.show = function()
+{
+	stroke(0);
+	noFill();
+	rect(this.x, this.y, this.w, this.w);
+
+	if(this.open)
+	{
+		if(this.bee)
+		{
+			fill(0);
+			ellipse(this.x + this.w*0.5, this.y + this.w*0.5, this.w * 0.5)
+		}
+		else
+		{
+			fill(169);
+			rect(this.x, this.y, this.w, this.w);
+			if (this.beesNext > 0){
+				textAlign(CENTER);
+				fill(0);
+				text(this.beesNext, this.x + this.w*0.5, this.y + this.w*0.5 - 3);
+			}
+		}
+	}
+	else
+	{
+		if (this.flag)
+		{
+			fill(255, 0, 0);
+			rect(this.x + this.w*0.25, this.y +this.w*0.25, this.w*0.5, this.w*0.5);
+		}
+	}
+}
+
+Cell.prototype.countBees = function()
+{
+	if (this.bee)
+	{	
+		this.beesNext = -1;
+		return ;
+	}
+
+	var total = 0;
+	for (var xoff = -1; xoff <= 1; xoff++){
+		for (var yoff = -1; yoff <= 1; yoff++){
+			var i = this.i + xoff;
+			var j = this.j + yoff;
+
+			if (i > -1 && i < cols && j > -1 && j < rows)
+			{
+				var neighbor = grid[i][j];
+				if (neighbor.bee)
+				{
+					total++;
+				}
+			}
+		}
+	}
+	this.beesNext = total;
+}
+
+Cell.prototype.contains = function(x, y)
+{
+	return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w)
+}
+
+Cell.prototype.reveal = function(x, y)
+{
+	this.open = true;
+	if (this.beesNext == 0){
+		this.floodit();
+	}
+}
+
+Cell.prototype.floodit = function()
+{
+	for (var xoff = -1; xoff <= 1; xoff++){
+		for (var yoff = -1; yoff <= 1; yoff++){
+			var i = this.i + xoff;
+			var j = this.j + yoff;
+
+			if (i > -1 && i < cols && j > -1 && j < rows)
+			{
+				var neighbor = grid[i][j];
+				if (!neighbor.bee && !neighbor.open)
+				{
+					neighbor.reveal();
+				}
+			}
+		}
+	}
+}
---
+++ Minesweeper/game.js
@@ -0,0 +1,243 @@
+var cols, rows, tile_px, grid, totalBees, reset_button, size_slider, winTime, isWin, isLose, font, hasStarted, timer, fontSize = 20;
+
+
+oncontextmenu="event.preventDefault();"
+
+function reset()
+{
+	isWin = false;
+	isLose = false;
+	hasStarted = false;
+	cols = size_slider.value();
+	rows = size_slider.value();
+	tile_px = 26;
+	totalBees = Math.floor(cols*rows*0.125);
+
+	// create a canvas the size of the game board
+	createCanvas(cols * tile_px + 50, rows * tile_px + 50);
+
+
+	// create 2D array
+	grid = create2Darray(cols, rows);
+
+	// fill each tile in 2D array
+	for (var i = 0; i < cols; i++)
+	{
+		for (var j = 0; j < rows; j++)
+		{
+			grid[i][j] = new Cell(i, j, tile_px);
+		}
+	}
+
+	// Pick totalBees spots
+	var options = [];
+	for (var i = 0; i < cols; i++){
+		for (var j = 0; j < rows; j++){
+			options.push([i,j]);
+		}
+	}
+
+	for (var x = 0; x < totalBees; x++){
+		var index = floor(random(options.length));
+		var choice = options[index];
+		var i = choice[0];
+		var j = choice[1];
+		// Delete the spot where the bee was placed
+		options.splice(index, 1);
+		grid[i][j].bee = true;
+	}
+
+
+
+	// count neighboring bees
+	for (var i = 0; i < cols; i++)
+	{
+		for (var j = 0; j < rows; j++)
+		{
+			grid[i][j].countBees();
+		}
+	}
+}
+
+function preload()
+{
+	font = loadFont('heavy_data.ttf');
+}
+
+function setup()
+{
+	textFont(font);
+	textSize(fontSize);
+	textAlign(CENTER, CENTER);
+
+	size_slider = createSlider(8, 16, 8);
+
+	reset();
+
+	reset_button = createButton('Reset');
+	reset_button.position(10, 10);
+	reset_button.mousePressed(reset);
+
+	size_slider.position(65, 10);
+}	
+
+function create2Darray(c, r)
+{
+	// create array of rows x cols
+	var matrix = new Array(c);
+	for (var i = 0; i < r; i++)
+	{
+		matrix[i] = new Array(r);
+	}
+	return matrix;
+}
+
+function GameOver()
+{
+	isLose = true;
+
+	if (hasStarted)
+	{
+	var end = new Date();
+	endTime = (end.getTime() - timer.getTime()) / 1000;
+	hasStarted = false;
+	}
+
+	for (var i = 0; i < cols; i++)
+	{
+		for (var j = 0; j < rows; j++)
+		{
+			grid[i][j].open = true;
+		}
+	}
+}
+
+function checkForWin()
+{
+	var bee_count = 0;
+	var num_count = 0;
+
+	for (var i = 0; i < cols; i++)
+	{
+		for (var j = 0; j < rows; j++)
+		{
+			if (grid[i][j].bee && grid[i][j].flag && !grid[i][j].open)
+			{
+				bee_count++;
+			}
+			if (!grid[i][j].bee && grid[i][j].open)
+			{
+				num_count++;
+			}
+		}
+	}
+
+	if (bee_count == totalBees && num_count == (rows*cols)-totalBees)
+	{
+		if (hasStarted)
+		{
+		var end = new Date();
+		endTime = (end.getTime() - timer.getTime()) / 1000;
+		hasStarted = false;
+		}
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function mousePressed()
+{
+
+	if (!isWin && !isLose)
+	{
+		for (var i = 0; i < cols; i++)
+		{
+			for (var j = 0; j < rows; j++)
+			{
+				if (grid[i][j].contains(mouseX, mouseY))
+				{
+					if (!hasStarted) // start the game timer
+					{
+						hasStarted = true;
+						timer = new Date();
+					}
+
+					if (mouseButton == LEFT){
+						grid[i][j].reveal();
+					}
+
+					else if (mouseButton == RIGHT)
+					{
+						if (grid[i][j].flag == false)
+						{
+							grid[i][j].flag = true;
+						}
+						else if (grid[i][j].flag == true)
+						{
+							grid[i][j].flag = false;
+						}
+					}
+
+					if (grid[i][j].bee && grid[i][j].open)
+					{
+						GameOver();
+					}
+					else
+					{
+						isWin = checkForWin();
+					}
+				}
+			}
+		}
+	}
+}
+
+function draw()
+{
+	// set backround color
+	background(255);
+
+	// draw each tile
+	for (var i = 0; i < cols; i++)
+	{
+		for (var j = 0; j < rows; j++)
+		{
+			grid[i][j].show();
+		}
+	}
+
+	textAlign(CENTER);
+	fill(0);
+	text(size_slider.value(), 200, 10);
+
+	if (isWin)
+	{
+		textAlign(CENTER);
+		fill(0);
+		text('GAME!', rows*tile_px - 20, cols*tile_px + 40);
+		textAlign(LEFT);
+		fill(0);
+		text(endTime, 3, cols*tile_px + 40);
+	}
+	else if(isLose)
+	{
+		textAlign(CENTER);
+		fill(0);
+		text('LOSE!', rows*tile_px - 20, cols*tile_px + 40);
+		textAlign(LEFT);
+		fill(0);
+		text(endTime, 3, cols*tile_px + 40);
+	}
+	else if(hasStarted)
+	{
+		var curr_time = new Date();
+		var elapsed_time = curr_time.getTime() - timer.getTime();
+		textAlign(LEFT);
+		fill(255);
+		text(elapsed_time / 1000, 3, cols*tile_px + 40);
+	}
+}
