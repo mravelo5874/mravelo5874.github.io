d32e8129ada93f9c5daf79c92b578a5e5afe046e
Marco Ravelo <mravelo5874@gmail.com>
2023-04-26 00:02:14 -0500

    quick fix :hearts:

--- 2021-website/scripts/app.js
+++
@@ -1,5 +0,0 @@
-/*eslint-env browser*/
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
